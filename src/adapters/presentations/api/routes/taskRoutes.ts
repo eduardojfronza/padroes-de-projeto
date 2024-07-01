6f32df429e996e2e9deca541f26ebd8fcd4a9e9a
Eduardo <edujfronza@gmail.com>
2024-07-01 09:58:02 -0300

    refactor - inserindo a task no banco

--- src/adapters/presentations/api/routes/taskRoutes.ts
+++ src/adapters/presentations/api/routes/taskRoutes.ts
@@ -2,10 +2,17 @@ import { Router } from "express";
 import { AddTaskController } from "../../../controllers/task/addTask";
 import { expressRouteAdapter } from "../../../expressRouteAdapter";
 import { DateValidatorAdapter } from "../../../dateValidatorAdapter";
+import { DbAddTask } from "../../../../dataSources/db/dbAddTask";
+import { TaskMongoRepository } from "../../../../dataSources/db/repository/taskMongoRepository";
 
 export default (router: Router): void => {
   const dateValidatorAdapter = new DateValidatorAdapter();
+  const taskMongoRepository = new TaskMongoRepository();
+  const dbAddTask = new DbAddTask(taskMongoRepository);
+  const addTaskController = new AddTaskController(
+    dbAddTask,
+    dateValidatorAdapter
+  );
 
-  const addTaskController = new AddTaskController(dateValidatorAdapter);
   router.post("/tasks", expressRouteAdapter(addTaskController));
 };
--- src/dataSources/db/repository/taskMongoRepository.ts
+++ src/dataSources/db/repository/taskMongoRepository.ts
@@ -1,9 +1,26 @@
 import { Task } from "../../../entities/task";
 import { AddATaskModel } from "../../../usecases/addTask";
 import { AddTaskRepository } from "../../../usecases/repository/addTaskRepository";
+import { MongoManager } from "../../config/mongoManager";
 
 export class TaskMongoRepository implements AddTaskRepository {
-  add(taskData: AddATaskModel): Promise<Task> {
-    throw new Error("Method not implemented.");
+  async add(taskData: AddATaskModel): Promise<Task> {
+    const taskCOllection = MongoManager.getInstance().getCollection("tasks");
+    const { insertedId } = await taskCOllection.insertOne(taskData);
+
+    const taskById = await taskCOllection.findOne({ _id: insertedId });
+
+    if (!taskById) {
+      throw new Error("Task not found");
+    }
+
+    const task: Task = {
+      id: taskById._id.toHexString(),
+      title: taskById.title,
+      description: taskById.description,
+      date: taskById.date,
+    };
+
+    return task;
   }
 }
