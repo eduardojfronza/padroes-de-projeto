94a0349174792ed34f067e7d51d9424d8f376831
Eduardo <edujfronza@gmail.com>
2024-07-02 14:42:28 -0300

    reafactor - criando um decorator para o controller

---
+++ src/adapters/factories/taskControllerFactory.ts
@@ -0,0 +1,15 @@
+import { DbAddTask } from "../../dataSources/db/dbAddTask";
+import { TaskMongoRepository } from "../../dataSources/db/repository/taskMongoRepository";
+import { AddTaskController } from "../controllers/task/addTask";
+import { DateValidatorAdapter } from "../dateValidatorAdapter";
+import { LogErrorControllerDecorator } from "../decorators/logErrorControllerDecorator";
+
+export const taskControllerFactory = () => {
+  const dateValidatorAdapter = new DateValidatorAdapter();
+  const taskMongoRepository = new TaskMongoRepository();
+
+  const dbAddTask = new DbAddTask(taskMongoRepository);
+  const taskController = new AddTaskController(dbAddTask, dateValidatorAdapter);
+
+  return new LogErrorControllerDecorator(taskController);
+};
